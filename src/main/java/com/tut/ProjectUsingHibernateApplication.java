fc0e1d1fddf71258c271f1bcb23272914a867d1a
Nishthakumari <knishtha0@gmail.com>
2023-05-29 16:23:52 +0530

    First Hibernate Program using Annotations | how to save data using hibernate

--- src/main/java/com/tut/ProjectUsingHibernateApplication.java
+++ src/main/java/com/tut/ProjectUsingHibernateApplication.java
@@ -1,6 +1,8 @@
 package com.tut;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class ProjectUsingHibernateApplication {
@@ -13,8 +15,20 @@ public class ProjectUsingHibernateApplication {
 		cfg.configure();
 		SessionFactory factory = cfg.buildSessionFactory();
 
-		System.out.println(factory);
-		System.out.println(factory.isClosed());
+		//creating student
+
+		Student st =new Student();
+		st.setId(102);
+		st.setName("Nishtha");
+		st.setCity("Lucknow");
+		System.out.println(st);
+
+		Session session = factory.openSession();
+
+		Transaction tx = session.beginTransaction();
+		session.save(st);
+		tx.commit();
+		session.close();
 	}
 
 }
