b99145dcca061f3932b2fdb467c8c97f156eae56
Nishthakumari <knishtha0@gmail.com>
2023-05-29 15:27:45 +0530

    Configuring Hibernate in our project | hibernate configuration | hibernate.cfg.xml

--- src/main/java/com/tut/ProjectUsingHibernateApplication.java
+++ src/main/java/com/tut/ProjectUsingHibernateApplication.java
@@ -1,10 +1,20 @@
 package com.tut;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class ProjectUsingHibernateApplication {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello world");
+
+		Configuration cfg =new Configuration();
+		cfg.configure();
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		System.out.println(factory);
+		System.out.println(factory.isClosed());
 	}
 
 }
