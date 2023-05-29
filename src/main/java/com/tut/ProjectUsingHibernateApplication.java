c1e369767334e10be2be3eb90001bca628054bf6
Nishthakumari <knishtha0@gmail.com>
2023-05-29 19:10:49 +0530

    Saving Image and other type value with hibernate

--- src/main/java/com/tut/ProjectUsingHibernateApplication.java
+++ src/main/java/com/tut/ProjectUsingHibernateApplication.java
@@ -5,9 +5,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.util.Date;
+
 public class ProjectUsingHibernateApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		System.out.println("Hello world");
 
@@ -23,10 +26,25 @@ public class ProjectUsingHibernateApplication {
 		st.setCity("Lucknow");
 		System.out.println(st);
 
+		Address ad = new Address();
+		ad.setStreet("street1");
+		ad.setCity("Delhi");
+		ad.setOpen(true);
+		ad.setAddedDate(new Date());
+		ad.setX(123.123);
+
+		//Reading image
+//		FileInputStream fis = new FileInputStream("src/main/java/me.jpeg");
+//		byte[] data = new byte[fis.available()];
+//		fis.read(data);
+//		ad.setImage(data);
+
+
 		Session session = factory.openSession();
 
 		Transaction tx = session.beginTransaction();
 		session.save(st);
+		session.save(ad);
 		tx.commit();
 		session.close();
 	}
