faabd6d25a1c21453317d0addbf314ebeb09a7cd
Nishthakumari <knishtha0@gmail.com>
2023-05-30 15:38:13 +0530

    Implementing Pagination using Hibernate HQL

--- src/main/java/com/hql/HQLExample.java
+++ src/main/java/com/hql/HQLExample.java
@@ -3,9 +3,11 @@ package com.hql;
 import com.tut.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class HQLExample {
@@ -34,6 +36,38 @@ public class HQLExample {
         }
 
 
+        System.out.println("_______________________________________________________________________");
+
+        Transaction tx = s.beginTransaction();
+//        Query q2 = s.createQuery("delete from Student s where s.city=:c");
+//        q2.setParameter("c", "ABC");
+
+        //delete query
+//        int r =q2.executeUpdate();
+//        System.out.println("Deleted");
+//        System.out.println(r);
+//        tx.commit();
+
+        //update query
+
+        Query q2 = s.createQuery("update Student set city=:c where name=:n");
+        q2.setParameter("c", "Delhi");
+        q2.setParameter("n", "Peter");
+        int r = q2.executeUpdate();
+        System.out.println(r + "  Object updated");
+        tx.commit();
+
+        Query q3 = s.createQuery("select q.question, q.questionId, a.answer from Question " +
+                "as q INNER JOIN q.answers as a ");
+
+
+         List<Object[]> list3 = q3.getResultList();
+
+        for(Object[] arr: list3)
+        {
+
+            System.out.println(Arrays.toString(arr));
+        }
         s.close();
         factory.close();
     }
